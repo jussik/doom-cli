6e999d47b3f4026cf80aa998ef8b97d819bdef8b
Jussi Kosunen <deathcarrot@gmail.com>
2025-03-01 09:35:15 +0200

    fix warning + publish props into csproj

--- DoomCli/CommonSettings.cs
+++ DoomCli/CommonSettings.cs
@@ -5,10 +5,12 @@ namespace DoomCli;
 
 public class CommonSettings : CommandSettings
 {
+    private const string DefaultConfigPath = @"%USERPROFILE%\.DoomCli.json";
+
     [Description("Path to the configuration file")]
     [CommandOption("--config-file")]
-    [DefaultValue(@"%USERPROFILE%\.DoomCli.json")]
-    public string ConfigurationFile { get; set; }
+    [DefaultValue(DefaultConfigPath)]
+    public string ConfigurationFile { get; set; } = DefaultConfigPath;
     
     [Description("Set working directory to the directory of the executable, useful for executing from a custom protocol link")]
     [CommandOption("--relative-to-exe")]
